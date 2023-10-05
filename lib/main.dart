ae49b40ae7a87e593d1196e8c79b5c5153c8a5fe
Kurogoma939 <kan.sss.939@gmail.com>
2023-10-06 03:28:35 +0900

    ベースのヒートマップ完成

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,226 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(title: const Text("GitHub Heatmap")),
+        body: Heatmap(
+          data: [
+            // テストデータ
+            {DateTime(2023, 1, 1): 1},
+            {DateTime(2023, 1, 2): 2},
+            {DateTime(2023, 1, 3): 6},
+            // 5月データ
+            {DateTime(2023, 5, 1): 5},
+            {DateTime(2023, 5, 2): 4},
+            {DateTime(2023, 5, 3): 4},
+            {DateTime(2023, 5, 4): 9},
+            {DateTime(2023, 5, 5): 6},
+            // 8月データ
+            {DateTime(2023, 8, 1): 5},
+            {DateTime(2023, 8, 2): 10},
+            {DateTime(2023, 8, 3): 11},
+            {DateTime(2023, 8, 4): 4},
+            // 12月データ
+            {DateTime(2023, 12, 1): 1},
+            {DateTime(2023, 12, 2): 10},
+            {DateTime(2023, 12, 3): 4},
+            {DateTime(2023, 12, 4): 10},
+            // ... 他のデータ
+          ],
+          colorSet: const {
+            1: Color(0xFFD7ECBF),
+            3: Color(0xFFB3E778),
+            5: Color(0xFF239C27),
+            10: Color(0xFF027C06),
+          },
+          cellSize: 16.0,
+          padding: const EdgeInsets.all(4.0),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Heatmap extends StatefulWidget {
+  final List<Map<DateTime, int>> data;
+  final Map<int, Color> colorSet;
+  final double cellSize;
+  final EdgeInsets padding;
+  final Color? defaultColor;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const Heatmap({
+    super.key,
+    required this.data,
+    required this.colorSet,
+    this.cellSize = 16.0,
+    this.padding = const EdgeInsets.all(0.0),
+    this.defaultColor,
+  });
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HeatmapState createState() => _HeatmapState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HeatmapState extends State<Heatmap> {
+  DateTime? _selectedDate;
+  late OverlayEntry _overlayEntry;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Padding(
+        padding: widget.padding,
+        child: GestureDetector(
+          onLongPress: () {
+            // PopupMenuを表示
+            _showPopupMenu(context);
+          },
+          onLongPressUp: () {
+            if (_overlayEntry.mounted) {
+              _overlayEntry.remove();
+            }
+          },
+          child: SizedBox(
+            height: (widget.cellSize * 7) + widget.padding.vertical,
+            child: CustomPaint(
+              painter: HeatmapPainter(
+                  data: widget.data,
+                  colorSet: widget.colorSet,
+                  cellSize: widget.cellSize,
+                  onTapCell: (date) {
+                    setState(() {
+                      _selectedDate = date;
+                    });
+                  }),
+              size: Size(
+                  widget.cellSize * (365 ~/ 7) + widget.padding.horizontal,
+                  (widget.cellSize * 7) + widget.padding.vertical),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _showPopupMenu(BuildContext context) {
+    if (_selectedDate == null) return;
+
+    var overlay = Overlay.of(context);
+    var overlayPosition = OverlayEntry(
+      builder: (context) => Positioned(
+        top: 140, // 位置は適切に調整してください。
+        left: MediaQuery.sizeOf(context).width * 0.4, // 位置は適切に調整してください。
+        child: Material(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(4.0),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black12,
+                  offset: Offset(0, 2),
+                  blurRadius: 6.0,
+                ),
+              ],
+            ),
+            child: Center(
+              child: Text(
+                _selectedDate!.toLocal().toIso8601String().split('T').first,
+                style: const TextStyle(fontSize: 12.0),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    overlay.insert(overlayPosition);
+    _overlayEntry = overlayPosition;
+  }
+}
+
+class HeatmapPainter extends CustomPainter {
+  final List<Map<DateTime, int>> data;
+  final Map<int, Color> colorSet;
+  final double cellSize;
+  final void Function(DateTime date)? onTapCell;
+  final Color? defaultColor;
+
+  HeatmapPainter({
+    this.data = const [],
+    this.colorSet = const {},
+    this.cellSize = 16.0,
+    this.onTapCell,
+    this.defaultColor,
+  });
+
+  @override
+  bool? hitTest(Offset position) {
+    // セルの位置を計算
+    int week = (position.dx ~/ cellSize).toInt();
+    int day = (position.dy ~/ cellSize).toInt();
+
+    DateTime selectedDate = DateTime(DateTime.now().year, 1, 1)
+        .add(Duration(days: (week * 7) + day));
+    onTapCell!(selectedDate);
+
+    return super.hitTest(position);
+  }
+
+  Color _getColorFromSet(int value) {
+    List<int> thresholds = colorSet.keys.toList()..sort();
+    for (var threshold in thresholds) {
+      if (value <= threshold) return colorSet[threshold]!;
+    }
+    return defaultColor ?? Colors.grey[300]!;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    double borderThickness = 0.5;
+    DateTime currentYearStartDate = DateTime(DateTime.now().year, 1, 1);
+
+    for (int week = 0; week < (365 ~/ 7); week++) {
+      for (int day = 0; day < 7; day++) {
+        double x = week * cellSize;
+        double y = day * cellSize;
+
+        Map<DateTime, int>? matchingData = data.firstWhere(
+            (item) => item.containsKey(
+                currentYearStartDate.add(Duration(days: (week * 7) + day))),
+            orElse: () => {});
+
+        Color cellColor = Colors.grey[300]!;
+        if (matchingData.isNotEmpty) {
+          int value = matchingData.values.first;
+          cellColor = _getColorFromSet(value);
+        }
+
+        canvas.drawRect(
+          Rect.fromPoints(
+            Offset(x, y),
+            Offset(
+                x + cellSize - borderThickness, y + cellSize - borderThickness),
+          ),
+          Paint()..color = cellColor,
+        );
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
